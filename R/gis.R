c27af62784ab99a59594c4d0aa7f86bf9e4f0ea9
Blake Massey <blakemassey@gmail.com>
2020-05-28 20:53:48 -0400

    Made updates to several functions.

--- R/gis.R
+++ R/gis.R
@@ -961,6 +961,63 @@ CreateRasterNestConDist <- function(years,
   return(nest_con_dist)
 }
 
+
+
+#' Creates a Raster of probability values based on Conspecific/Nest distance
+#'     and adjusted based on the agent's current location
+#'
+#' @param con_nest_raster raster, conspecific and nest distance value raster
+#' @param raster_extent extent, extent of the output raster
+#' @param pars_gamma pars_gamma list (contains shape and rate)
+#' @param pars_rescale pars_rescale list (contains y_min, y_max, y_min_new,
+#'     y_max_new
+#' @param x integer x coordinate
+#' @param y integer y coordinate
+#' @param base raster, base
+#'
+#' @return Raster
+#' @export
+#'
+CreateRasterConNestDistProb <- function(con_nest_raster,
+                                        raster_extent,
+                                        pars_gamma,
+                                        pars_rescale,
+                                        x,
+                                        y,
+                                        base){
+  gamma_shape <- pars_gamma$shape
+  gamma_rate <- pars_gamma$rate
+  y_min <- pars_rescale$y_min
+  y_max <- pars_rescale$y_max
+  y_min_new <- pars_rescale$y_min_new
+  y_max_new <- pars_rescale$y_max_new
+
+  cellsize <- raster::res(base)[1]
+
+  plot(con_nest_raster)
+  con_nest_crop <- crop(con_nest_raster, raster_extent, snap = 'in')
+  plot(con_nest_crop)
+
+  xy <- CenterXYInCell(x, y, raster::xmin(base), raster::ymin(base),
+    raster::res(base)[1])  # May be unnecessary
+  xy_pt <- data.frame(x = xy[1], y = xy[2])
+  xy_con_nest <- extract(con_nest_crop, xy_pt)
+
+  con_nest_centered <- calc(con_nest_crop, fun=function(x){(x - xy_con_nest)/1000})
+  plot(con_nest_centered)
+  y_diff_new <- y_max_new - y_min_new
+
+  y_pgamma <- pgamma(xy_con_nest/1000, shape=gamma_shape, rate=gamma_rate)
+  y_log_scale <- y_min_new + (((y_pgamma-y_min)/(y_max-y_min)) * (y_diff_new))
+
+#  curve(LogisticByInflection(x, inflection=0, scale=xy_log_scale), -15, 15)
+  LogisticByInflection2 <- function(x){
+    x <- LogisticByInflection(x, inflection=0, scale=y_log_scale)
+  }
+  con_nest_prob <- calc(con_nest_centered, fun=LogisticByInflection2)
+  return(con_nest_prob)
+}
+
 #' CreateSpatialLines
 #'
 #' A wrapper function for creating a 'SpatialLines' object
